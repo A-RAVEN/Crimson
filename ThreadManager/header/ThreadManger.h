408ee65d78d8cc84aa13d54d5f75a249b5337d23
A-RAVEN <769561232@qq.com>
2023-07-01 11:30:04 +0800

    add taskgraph

--- ThreadManager/header/ThreadManger.h
+++ ThreadManager/header/ThreadManger.h
@@ -7,13 +7,15 @@ namespace thread_management
 	class CTask
 	{
 	public:
-		virtual CTask* Succeed(CTask const* CTask);
-		virtual CTask* Name(std::string name);
+		virtual CTask* Succeed(CTask const* CTask) = 0;
+		virtual CTask* Name(std::string name) = 0;
+		virtual CTask* Functor(std::function<void()> functor) = 0;
 	};
 
 	class CTaskGraph
 	{
 	public:
+		virtual CTask* NewTask() = 0;
 
 	};
 
@@ -22,5 +24,6 @@ namespace thread_management
 	public:
 		virtual void InitializeThreadCount(uint32_t threadNum) = 0;
 		virtual std::future<int> EnqueueAnyThreadWorkWithPromise(std::function<void()> function) = 0;
+		virtual void ExecuteTaskGraph(CTaskGraph* )
 	};
 }
